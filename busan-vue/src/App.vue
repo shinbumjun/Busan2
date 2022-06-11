b0c86944e91bb21af9851daf4b1beedf2e9cff63
shinbumjun <https://github.com/shinbumjun>
2022-06-11 11:31:15 +0900

    세팅

---
+++ busan-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <nav_bar></nav_bar>
+    <router-view></router-view>    
+    <Main_footer></Main_footer>
+  </div>
+</template>
+
+<script>
+import footer from './components/include/footer.vue'
+import nav_bar from './components/include/nav_bar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'Main_footer': footer,
+    'nav_bar': nav_bar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ busan-vue/src/router/index.js
@@ -0,0 +1,78 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import BusanCommunity from '../components/BusanCommunity.vue';
+import BusanCommunityinsertform from '../components/BusanCommunityinsertform.vue';
+import BusanCommunitydetail from '../components/BusanCommunitydetail.vue';
+import BusanFavorites from '../components/BusanFavorites.vue';
+import BusanTouristspotdetails from '../components/BusanTouristspotdetails.vue';
+import BusanTouristspot from '../components/BusanTouristspot.vue';
+import BusanCourse from '../components/BusanCourse.vue';
+import BusanHome from '../components/BusanHome.vue';
+import BusanLog_in from '../components/BusanLog_in.vue';
+import BusanMypage from '../components/BusanMypage.vue';
+import BusanRecommand_result from '../components/BusanRecommand_result.vue';
+import BusanRecommand_select from '../components/BusanRecommand_select.vue';
+import BusanSign_up from '../components/BusanSign_up.vue';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    mode: 'history',
+    scrollBehavior() { 
+        return { x: 0, y: 0 } 
+    },
+    routes: [
+        {
+            path: '/',
+            component: BusanHome
+        },
+        {
+            path: '/login',
+            component: BusanLog_in
+        },
+        {
+            path: '/signup',
+            component: BusanSign_up
+        },
+        {
+            path: '/mypage',
+            component: BusanMypage
+        },
+        {
+            path: '/recommand_result',
+            component: BusanRecommand_result
+        },
+        {
+            path: '/recommand_select',
+            component: BusanRecommand_select
+        },
+        {
+            path: '/community',
+            component: BusanCommunity
+        },
+        {
+            path: '/communityinsertform',
+            component: BusanCommunityinsertform
+        },
+        {
+            path: '/communitydetail',
+            component: BusanCommunitydetail
+        },
+        {
+            path: '/favorites',
+            component: BusanFavorites
+        },
+        {
+            path: '/touristspotdetails',
+            component: BusanTouristspotdetails
+        },
+        {
+            path: '/touristspot',
+            component: BusanTouristspot
+        },
+        {
+            path: '/course',
+            component: BusanCourse
+        },
+    ]   
+})
